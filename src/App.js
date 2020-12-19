4ed8f2a49f77d0ba1f483378492daf1b9cefdf74
manoj mohan <jesuismanoj@gmail.com>
2020-12-19 12:16:00 +0530

    Add row add function

--- src/App.js
+++ src/App.js
@@ -69,15 +69,42 @@ class App extends React.Component {
     )
   }
 
+  handleAddButtonClick = () => {
+
+  }
+
+  renderEmptyRow = () => {
+    return (
+      <tr>
+        <td><input></input></td>
+        {this.state.availableLanguages.map((lang) => <td><input/></td>)}
+      </tr>
+    )
+  }
+
 
   renderAddButton = () => {
-    return (<button title="add">
+    return (<button title="add" onClick={this.handleAddButtonClick}>
       add
     </button>)
   }
 
   handleSave = async () => {
 
+    let request = {
+      method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+      // mode: 'cors', // no-cors, *cors, same-origin
+      // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      // credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // redirect: 'follow', // manual, *follow, error
+      // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(this.state.translations)// JSON.stringify(data) // body data type must match "Content-Type" header
+    }
+    let response = await (await fetch(BASE_URL+"/translation", request)).json()
+    console.log("response of save ", response)
   }
 
   renderSaveButton = () => {
@@ -86,11 +113,24 @@ class App extends React.Component {
     </button>)
   }
 
+  childSave = (childState, clearChild) => {
+    let translationClone = {...this.state.translations}
+    for(let i=0; i< this.state.availableLanguages.length; i++){
+      translationClone[this.state.availableLanguages[i]][childState.constant] = childState[this.state.availableLanguages[i]]
+    }
+    this.setState({translations: translationClone})
+    console.log("translation clone ", translationClone)
+    clearChild()
+  }
+
   renderTable = () => {
     return (
       <table>
       {this.renderTitle()}
       {this.renderData()}
+      {/* {this.renderEmptyRow()} */}
+      <EmptyRow availableLanguages={this.state.availableLanguages} translations={this.state.translations} childSave={this.childSave}/>
+
     </table>
     )
   }
@@ -107,10 +147,58 @@ class App extends React.Component {
     return (
       <div className="App">
         {this.constants ? this.renderTable() : this.noDataView()}
-        {this.renderAddButton()}
         {this.renderSaveButton()}
       </div>
     );
+  }
+}
+
+class EmptyRow extends React.Component {
+
+
+  constructor(props){
+    super(props)
+    console.log("child props", props)
+    this.languageTextState = {}
+    for(let i=0;i< props.availableLanguages.length; i++){
+      this.languageTextState[props.availableLanguages[i]] = ""
+    }
+    this.state = {constant: "", ...this.languageTextState}
+  }
+
+  
+  clearChildState = () => {
+    for(let i=0;i< this.props.availableLanguages.length; i++){
+      this.languageTextState[this.props.availableLanguages[i]] = ""
+    }
+    this.setState({constant: "", ...this.languageTextState})
+  }
+
+  handleOnChangeConstant = (event) => {
+    this.setState({constant: event.target.value})
+  }
+
+  handleOnChange = (event, lang) => {
+    let newState = {}
+    newState[lang] = event.target.value
+    console.log("new state onchange", newState)
+    this.setState(newState)
+  }
+
+  handleOnClickAddNew = () => {
+    this.props.childSave(this.state, this.clearChildState)
+  }
+
+  render() {
+
+    console.log("state in child", this.state)
+    return (
+      <tr>
+        <td><input value={this.state.constant} onChange={this.handleOnChangeConstant}></input></td>
+        {this.props.availableLanguages.map((lang) => <td><input value={this.state[lang]} onChange={(event) => this.handleOnChange(event, lang)}/></td>)}
+        <td><button onClick={this.handleOnClickAddNew}> Add New </button></td>
+      </tr>
+    )
   }
 }
 
